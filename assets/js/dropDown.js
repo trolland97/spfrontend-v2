1ea32f467853acaf0411df848ff9e7a0e01107a6
trolland97 <trolland97@gmail.com>
2023-03-01 10:10:41 +0100

    fixes

--- assets/js/dropDown.js
+++ assets/js/dropDown.js
@@ -1,16 +1,8 @@
-function dropDown() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
-
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-          var openDropdown = dropdowns[i];
-      if(openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-      }
-    }
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
 }
